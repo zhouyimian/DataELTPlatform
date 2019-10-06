da449b1d496fc9055e32c757e3bae9c7502ef5a7
zhouyimian <313976009@qq.com>
2019-10-06 22:53:29 +0800

    Initial commit

---
+++ src/main/java/com/km/dataeltplatform/DataeltplatformApplication.java
@@ -0,0 +1,18 @@
+package com.km.dataeltplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+
+@SpringBootApplication(exclude = {
+        DataSourceAutoConfiguration.class
+})
+public class DataeltplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataeltplatformApplication.class, args);
+	}
+
+}
